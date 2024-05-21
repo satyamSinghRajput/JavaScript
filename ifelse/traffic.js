97348e48ac59e5f6027d31c77dc5d2ce32cee8ff
Satyam Kumar <logontosatyam@gmail.com>
2024-05-21 16:59:50 +0530

    trafic based problem

---
+++ ifelse/traffic.js
@@ -0,0 +1,14 @@
+let color = "Green";
+color = color.toLowerCase();
+if (color === "green"){
+    console.log("Go")
+}
+else if (color === "Yellow"){
+    console.log("Slow Down")
+}
+else if(color === "red"){
+    console.log("stop")
+}
+else{
+    console.log("Invalid color.");  
+}
