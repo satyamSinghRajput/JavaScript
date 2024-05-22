b457d5e6084b2a61951f7615863da57b1938db81
Satyam Kumar <logontosatyam@gmail.com>
2024-05-22 12:17:42 +0530

    Code Review and changes for Week and Traffic page

--- ifelse/traffic.js
+++ ifelse/traffic.js
@@ -1,14 +1,14 @@
-let color = "Green";
+let color = "GrEen";
 color = color.toLowerCase();
-if (color === "green"){
+if (color === "green") {
     console.log("Go")
 }
-else if (color === "Yellow"){
+else if (color === "yellow") {
     console.log("Slow Down")
 }
-else if(color === "red"){
+else if (color === "red") {
     console.log("stop")
 }
-else{
-    console.log("Invalid color.");  
+else {
+    console.log("Invalid color.");
 }
