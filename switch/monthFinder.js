68df837c9fe807c21c3029313fefd3dae8372df7
Satyam Kumar <logontosatyam@gmail.com>
2024-05-22 17:52:41 +0530

    Month and day finder based on Switch case

---
+++ switch/monthFinder.js
@@ -0,0 +1,54 @@
+let month = 12;
+
+switch(month){
+    case 1:
+        month = "January";
+        console.log("In " + month + " 31 days");
+        break;
+    case 2:
+        month = "Febaruary";
+        console.log("In " + month + " 28 days");
+        break;
+    case 3:
+        month = "March";
+        console.log("In " + month + " 31 days");
+        break;
+    case 4:
+        month = "April";
+        console.log("In " + month + " 30 days");
+        break;
+    case 5:
+        month = "May";
+        console.log("In " + month + " 31 days");
+        break;
+    case 6:
+        month = "June";
+        console.log("In " + month + " 30 days");
+        break;
+    case 7:
+        month = "July";
+        console.log("In" + month + " 31 days");
+        break;
+    case 8:
+        month = "August";
+        console.log("In" + month + " 31 days");
+        break;
+    case 9:
+        month = "September";
+        console.log("In " + month + " 30 days");
+        break;            
+    case 10:
+        month = "October";
+        console.log("In " + month + " 31 days");
+        break;
+    case 11:
+        month = "November";
+        console.log("In " + month + " 30 days");
+        break;
+    case 12:
+        month = "December";
+        console.log("In " + month + " 31 days");
+        break;
+    default:
+        console.log("This is invalid.Please select from 1 to 12.")          
+}
